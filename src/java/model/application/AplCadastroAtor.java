60be2aee306dede04efe9f8667ba9b4c53a959c7
Alexsander Beninca Gonçalves <alexis22pb@gmail.com>
2025-08-11 22:30:11 -0300

    Servlet e MVC criadas

---
+++ src/java/model/application/AplCadastroAtor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.application;
+
+import model.domain.ator;
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public class AplCadastroAtor {
+     public List<ator> listarTodos() {
+        // consulta no banco e retorna lista de atores
+        
+        
+        return null;
+    }
+    public void inserir(ator ator) {
+        // insere ator no banco
+    }
+    public void excluir(int id) {
+        // exclui ator do banco
+    }
+    public void atualizar(ator ator) {
+        // atualiza ator no banco
+    }
+}
